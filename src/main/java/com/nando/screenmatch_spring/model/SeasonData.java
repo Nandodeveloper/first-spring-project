8b0b7c406858add549cd9ef6930fded499af6d63
Fernando <fernandinhomoraesbr@gmail.com>
2025-03-18 20:31:12 -0300

    implements SeasonData class

---
+++ src/main/java/com/nando/screenmatch_spring/model/SeasonData.java
@@ -0,0 +1,11 @@
+package com.nando.screenmatch_spring.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record SeasonData(@JsonAlias("Season") Integer season,
+                         @JsonAlias("Episodes") List<EpisodeData> episodeDataList) {
+}
