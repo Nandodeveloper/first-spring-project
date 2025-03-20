e9ca595ff93c428604a995d5fcc308fe53326c24
Fernando <fernandinhomoraesbr@gmail.com>
2025-03-19 21:43:31 -0300

    implements a "main" class (not main) to run a menu for a user.

---
+++ src/main/java/com/nando/screenmatch_spring/main/Main.java
@@ -0,0 +1,50 @@
+package com.nando.screenmatch_spring.main;
+
+import com.nando.screenmatch_spring.exception.TitleNotFoundException;
+import com.nando.screenmatch_spring.model.SeasonData;
+import com.nando.screenmatch_spring.model.SeriesData;
+import com.nando.screenmatch_spring.service.APIConsumption;
+import com.nando.screenmatch_spring.service.ConvertsData;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    Scanner scanner = new Scanner(System.in);
+    private final String ADDRESS = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=e0ffa84d";
+    private String seriesName;
+    private APIConsumption apiConsumption = new APIConsumption();
+    private ConvertsData convertsData = new ConvertsData();
+
+    public void runMenu() {
+        System.out.println("Enter the name series for the search:");
+        seriesName = scanner.nextLine();
+
+        try {
+            var json = apiConsumption.getData(ADDRESS + this.seriesName.replace(" ", "+") + API_KEY);
+
+            SeriesData seriesData = convertsData.convertData(json, SeriesData.class);
+            if(seriesData.title() == null) {
+                throw new TitleNotFoundException("Title not found.");
+            }
+            System.out.println(seriesData);
+
+            List<SeasonData> seasonDataList = new ArrayList<>();
+            for (int i = 1; i <= seriesData.totalSeasons(); i++) {
+                var jsonSeason = apiConsumption.getData(ADDRESS +
+                        this.seriesName.replace(" ", "+") +
+                        "&season=" + i + API_KEY);
+                SeasonData seasonData = convertsData.convertData(jsonSeason, SeasonData.class);
+                seasonDataList.add(seasonData);
+            }
+            seasonDataList.forEach(System.out::println);
+
+            seasonDataList.forEach(t -> t.episodeDataList().forEach(e -> System.out.println(e.title())));
+
+        } catch (TitleNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
