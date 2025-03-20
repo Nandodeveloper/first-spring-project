1c0e9d44ec1194e65462e62f0e41fedb8863b30a
Fernando <fernandinhomoraesbr@gmail.com>
2025-03-19 21:44:25 -0300

    clean the main (ScrennmatchSpringApplication.java)

--- src/main/java/com/nando/screenmatch_spring/ScreenmatchSpringApplication.java
+++ src/main/java/com/nando/screenmatch_spring/ScreenmatchSpringApplication.java
@@ -1,16 +1,9 @@
 package com.nando.screenmatch_spring;
 
-import com.nando.screenmatch_spring.model.EpisodeData;
-import com.nando.screenmatch_spring.model.SeasonData;
-import com.nando.screenmatch_spring.model.SeriesData;
-import com.nando.screenmatch_spring.service.APIConsumption;
-import com.nando.screenmatch_spring.service.ConvertsData;
+import com.nando.screenmatch_spring.main.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchSpringApplication implements CommandLineRunner {
@@ -21,26 +14,7 @@ public class ScreenmatchSpringApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) {
-		try {
-			var apiConsumption = new APIConsumption();
-			var convertsData = new ConvertsData();
-			var jsonSeries = apiConsumption.getData("https://www.omdbapi.com/?t=gilmore+girls&apikey=e0ffa84d");
-			SeriesData seriesData = convertsData.convertData(jsonSeries, SeriesData.class);
-			System.out.println(seriesData);
-			var jsonEp = apiConsumption.getData("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=e0ffa84d");
-			EpisodeData episodeData = convertsData.convertData(jsonEp, EpisodeData.class);
-			System.out.println(episodeData);
-
-			List<SeasonData> seasonDataList = new ArrayList<>();
-
-			for (int i = 1; i <= seriesData.totalSeasons(); i++) {
-				var jsonSeason = apiConsumption.getData("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=e0ffa84d");
-				SeasonData seasonData = convertsData.convertData(jsonSeason, SeasonData.class);
-				seasonDataList.add(seasonData);
-			}
-			seasonDataList.forEach(System.out::println);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+		Main main = new Main();
+		main.runMenu();
 	}
 }
